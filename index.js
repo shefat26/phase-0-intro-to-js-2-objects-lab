acf98957fda7c1475707a3bedcb4cec639802f15
shefat26 <shefatmoral@gmail.com>
2024-12-18 10:51:49 -0500

    Object prep

--- index.js
+++ index.js
@@ -1,1 +1,78 @@
 // Write your solution in this file!
+
+
+let employee = {
+
+    name:"John",
+
+    streetAddress: "30 w 15 street",
+
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+
+         return {
+            ...employee,
+
+            [key]: value
+         }
+}
+
+updateEmployeeWithKeyAndValue()
+
+
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+
+       
+            employee[key] = value;
+
+            return employee;
+         
+}
+
+destructivelyUpdateEmployeeWithKeyAndValue()
+
+
+//
+
+function  deleteFromEmployeeByKey(employee, key){
+
+       
+           let {[key]: value, ...newEmployee} = employee;
+
+           return newEmployee
+         
+}
+
+deleteFromEmployeeByKey()
+
+
+
+
+//
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key){
+
+       
+           delete employee[key];
+
+           return employee
+         
+}
+
+destructivelyDeleteFromEmployeeByKey()
+
+
+
+
+
+
+
+
+
+
+
